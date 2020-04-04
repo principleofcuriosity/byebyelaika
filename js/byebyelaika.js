12ea45f19f58f082718172a14e90ca19d61bc582
principleofcuriosity <61979878+principleofcuriosity@users.noreply.github.com>
2020-04-04 13:54:34 +0200

    Made my Rocket

--- js/byebyelaika.js
+++ js/byebyelaika.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 var changeState = function (state) {
 	document.body.className = 'body-state' + state;
 }
@@ -5,3 +6,42 @@ var changeState = function (state) {
 if (state == 2) {
 	document.getElementById('countdown').innerHTML() = 9;
 };
+=======
+var timer = null;
+var countdownNumber = 10;
+
+var changeState = function (state) {
+	document.body.className = 'body-state' + state;
+	clearInterval(timer);
+	countdownNumber = 10;
+	document.getElementById('countdown').innerHTML = countdownNumber;
+
+
+	// countdown
+	if (state == 2) {
+		timer = setInterval(function () {
+				countdownNumber = countdownNumber -1;
+				document.getElementById('countdown').innerHTML = countdownNumber;
+			if (countdownNumber <= 0) {
+				changeState(3);
+			};
+		}, 500);
+	} else if (state == 3) {
+		var success = setTimeout(function () {
+			var randomNumber = Math.round(Math.random()*8);
+
+			console.log('randomNumber:', randomNumber)
+
+			// success
+			if (randomNumber > 4) {
+				changeState(4);
+
+			// fail
+			} else {
+				changeState(5);
+			}
+		}, 2000);
+	}
+}
+
+>>>>>>> Stashed changes
